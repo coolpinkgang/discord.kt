96fc618da79a32f4edbae83ee52c785470458f20
Roman Gräf <romangraef@loves.dicksinhisan.us>
2020-09-23 23:40:30 +0200

    Auto Release Serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ group = "io.github.romangraef"
 val kotlinVersion = "1.4.10"
 val ktorVersion = "1.4.0"
 val kotlinxCoroutinesVersion = "1.3.9"
+val kotlinxSerializationVersion = "1.0.0-RC2"
 
 repositories {
     jcenter()
@@ -17,6 +18,11 @@ dependencies {
 	implementation(kotlin("stdlib-jdk8"))
 	implementation("io.ktor:ktor-server-netty:$ktorVersion")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinxSerializationVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
 }
 
+tasks.withType<Jar> {
+    archiveFileName.set("discord-kt.jar")
+}
 
