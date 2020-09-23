1646f0a762be25015bd1117c02b3a5a2a850ffd1
einsJannis <einsjannis@gmail.com>
2020-09-23 23:26:28 +0200

    Updated buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
-	kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.10"
+	kotlin("plugin.serialization") version "1.4.10"
 }
 
 version = "0.0.0a"
 group = "io.github.romangraef"
+val kotlinVersion = "1.4.10"
+val ktorVersion = "1.4.0"
+val kotlinxCoroutinesVersion = "1.3.9"
 
 repositories {
 
@@ -11,6 +15,8 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib-jdk8"))
+	implementation("io.ktor:ktor-server-netty:$ktorVersion")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
 }
 
 
