73c4eb977f46ec90de8a84fe7b2bf3765c9e831a
einsJannis <einsjannis@gmail.com>
2020-10-03 20:18:32 +0200

    Fixed Connection.kt

--- src/main/kotlin/io/github/romangraef/discordkt/models/user/Connection.kt
+++ src/main/kotlin/io/github/romangraef/discordkt/models/user/Connection.kt
@@ -3,6 +3,7 @@ package io.github.romangraef.discordkt.models.user
 import io.github.romangraef.discordkt.models.guild.Integration
 import io.github.romangraef.discordkt.models.serial.Snowflake
 import io.github.romangraef.discordkt.models.serial.SnowflakeMixin
+import kotlinx.serialization.SerialName
 
 import kotlinx.serialization.Serializable
 
@@ -14,7 +15,9 @@ data class Connection(
     val revoked: Boolean? = null,
     val integrations: List<Integration>? = null,
     val verified: Boolean,
+    @SerialName("friend_sync")
     val friendSync: Boolean,
+    @SerialName("show_activity")
     val showActivity: Boolean,
     val visibility: Visibility
     ) : SnowflakeMixin()
